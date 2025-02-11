bd57dbbb89d9f818390b0b2d7beffaa77aa0348c
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-02-11 20:53:24 -0300

    feat: add dashboard cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+import { DollarSign } from "lucide-react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="sm:ml-14 p-4">
+      <section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        {Array.from({ length: 7 }).map((_, i) => (
+          <Card key={i}>
+            <CardHeader>
+              <div className="flex items-center justify-center">
+                <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                  Vendas Totais
+                </CardTitle>
+
+                <DollarSign className="ml-auto w-4 h-4" />
+              </div>
+
+              <CardDescription>Total de vendas em 90 dias</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <p className="text-base sm:text-lg font-bold">40 mil em vendas</p>
+            </CardContent>
+          </Card>
+        ))}
+      </section>
+    </main>
+  );
 }
